4d021e9302e09c638ebd0cab46077ebf82b5c059
azizsaif899 <azizsaif.d@gmail.com>
2025-07-30 22:37:51 +0300

    del

--- kill-port.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo جاري البحث عن العمليات المشغولة للبورت %1...
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :%1') do (
-    echo قتل العملية %%a
-    taskkill /f /pid %%a
-)
-echo تم تحرير البورت %1
