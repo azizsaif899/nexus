319b38f72bff7b2f077f990e844653578dfc0293
azizsaif.d <azizsaif.d@gmail.com>
2025-08-19 15:30:10 +0000

    تحديث 2

---
+++ g-assistant-nx/apps/crm/index.tsx
@@ -0,0 +1,13 @@
+typescriptreact
+import React from 'react';
+
+const CRMApp: React.FC = () => {
+  return (
+    <div>
+      <h1>واجهة تطبيق CRM</h1>
+      {/* Add your CRM components here */}
+    </div>
+  );
+};
+
+export default CRMApp;
